38ab46bd79a994eeab104d615c04b69b36be544e
Niranga Lakshan <nirangawh@hotmail.com>
2024-07-07 01:38:41 +0530

    Change env values

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   MONGO_IP: process.env.MONGO_IP || "database-mongo",
   MONGO_PORT: process.env.MONGO_PORT || 27017,
-  MONGO_USER: process.env.MONGO_USER,
-  MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+  MONGO_USER: process.env.MONGO_INITDB_ROOT_USERNAME,
+  MONGO_PASSWORD: process.env.MONGO_INITDB_ROOT_PASSWORD,
   REDIS_URL: process.env.REDIS_URL || "redis",
   REDIS_PORT: process.env.REDIS_PORT || 6379,
   SESSION_SECRET: process.env.SESSION_SECRET,
